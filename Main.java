f783a0e2bdce1a3176f1afb5d8f8e6e8583da7ff
Adityanim <69639016+Adityanim@users.noreply.github.com>
2020-08-13 12:47:25 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Aditya Nimbalkar");
+		System.out.println("I attend Milpitas High School");
+		System.out.println("My dream school is University of Pennsylvania");
+		System.out.println("I am planning to major in Economics");
+		System.out.println("I recently started playing the guitar and I'm practicing to get better");
+	}
+}
